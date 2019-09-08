64b81a1fe06b7349c30323fe82e78f8c96fcdb94
snototter <h.possegger+dev@gmail.com>
2019-09-08 22:09:19 +0200

    adapt to config changes

--- helheimr/helheimr_deconz.py
+++ helheimr/helheimr_deconz.py
@@ -2,7 +2,7 @@ import json
 import logging
 import requests
 
-#TODO https://stackoverflow.com/questions/373335/how-do-i-get-a-cron-like-scheduler-in-python
+#TODO https://stackoverflow.com/questions/373335/how-do-i-get-a-cron-like-scheduler-in-python <== needed for controller
 
 class PlugState:
     def __init__(self, display_name, deconz_plug):
@@ -23,56 +23,65 @@ class TemperatureState:
 
 """ Communication with the zigbee/raspbee (deconz REST API) gateway """
 class DeconzWrapper:
-    def __init__(self, config):
+    def __init__(self, cfg):
         # Deconz parameters
-        self.gateway = config['raspbee']['deconz']['gateway']
-        self.tcp_port = config['raspbee']['deconz']['port']
-        self.token = config['raspbee']['deconz']['api_token']
+        self.gateway = cfg['raspbee']['deconz']['gateway']
+        self.tcp_port = cfg['raspbee']['deconz']['port']
+        self.token = cfg['raspbee']['deconz']['api_token']
         self.api_url = 'http://' + self.gateway + ':' + str(self.tcp_port) + '/api/' + self.token
 
-        #TODO
-        # # Currently I don't want a generic approach but rather know 
-        # # exactly (hardcoded) which plugs I control programatically
-        # self.heater_plug_mapping = dict()
-        # self.heater_plug_display_name_mapping = {
-        #         config['heater_plug_mappings']['tepidarium'][0] : config['heater_plug_mappings']['tepidarium'][1],
-        #         config['heater_plug_mappings']['flat'][0] : config['heater_plug_mappings']['flat'][1]
-        #     }
-        # self._map_plugs(self.heater_plug_mapping, self.heater_plug_display_name_mapping, [
-        #     config['heater_plug_mappings']['tepidarium'][0],
-        #     config['heater_plug_mappings']['flat'][0]
-        #     ])
+        # Currently I don't want a generic approach but rather know 
+        # exactly (hardcoded) which plugs I control programatically
+        
+        # Map deconz plug name to human-readable display name
+        self.heater_plug_display_name_mapping = {
+            cfg['raspbee']['heating']['plug_names']['flat'] : cfg['raspbee']['heating']['display_names']['flat'],
+            cfg['raspbee']['heating']['plug_names']['basement'] : cfg['raspbee']['heating']['display_names']['basement'],
+        }
 
-    # def lookup_heater_display_name(self, raspbee_id):
-    #     for lbl, rid in self.heater_plug_mapping.items():
-    #         if rid == raspbee_id:
-    #             return self.heater_plug_display_name_mapping[lbl]
-    #     return 'ID {}'.format(raspbee_id)
+        # Map deconz plug name to deconz ID
+        self.heater_plug_raspbee_name_mapping = _map_deconz_plugs(cfg)
 
-    # def _map_plugs(self, plug_mapping, display_name_mapping, plug_names):
-    #     # Currently, I'm only using smart plugs which are linked to
-    #     # the zigbee gateway as "lights"
-    #     r = requests.get(self._api_url() + '/lights')
-    #     lights = json.loads(r.content)
-    #     logger = logging.getLogger()
 
-    #     for raspbee_id, light in lights.items():
-    #         if light['name'] in plug_names:
-    #             logger.info('Mapping {:s} ({:s}) to RaspBee id {}'.format(light['name'],
-    #                 display_name_mapping[light['name']], raspbee_id))
-    #             plug_mapping[light['name']] = raspbee_id
+    def lookup_heater_display_name(self, raspbee_id):
+        for lbl, rid in self.heater_plug_mapping.items():
+            if rid == raspbee_id:
+                return self.heater_plug_display_name_mapping[lbl]
+        return 'ID {}'.format(raspbee_id)
+
+    
+    def _map_deconz_plugs(cfg):
+        # Our 'smart' plugs are linked to the zigbee gateway as "lights"
+        r = requests.get(self._api_url() + '/lights')
+        lights = json.loads(r.content)
+        logger = logging.getLogger()
+
+        plug_names = [
+            cfg['raspbee']['heating']['plug_names']['flat'],
+            cfg['raspbee']['heating']['plug_names']['basement']
+        ]
+        mapping = dict()
+
+        for raspbee_id, light in lights.items():
+            if light['name'] in plug_names:
+                logger.info('Mapping {:s} ({:s}) to RaspBee ID {}'.format(light['name'],
+                    self.heater_plug_display_name_mapping[light['name']], raspbee_id))
+                mapping[light['name']] = raspbee_id
+        return mapping
+
 
     def query_heating(self):
         status = list()
         is_heating = False
-        # logger = logging.getLogger()
-        # for plug_lbl, plug_id in self.heater_plug_mapping.items():
-        #     r = requests.get(self._api_url() + '/lights/' + plug_id)
-        #     state = PlugState(self.heater_plug_display_name_mapping[plug_lbl], json.loads(r.content))
-        #     status.append(state)
-        #     logger.info(state)
-        #     is_heating = is_heating or state.on
+        logger = logging.getLogger()
+        for plug_lbl, plug_id in self.heater_plug_raspbee_name_mapping.items():
+            r = requests.get(self._api_url() + '/lights/' + plug_id)
+            state = PlugState(self.heater_plug_display_name_mapping[plug_lbl], json.loads(r.content))
+            status.append(state)
+            logger.info(state)
+            is_heating = is_heating or state.on
         return is_heating, status
+
 
     def switch_light(self, light_id, turn_on):
         r = requests.put(self.api_url + '/lights/' + light_id + '/state', data='{:s}'.format('{"on":true}' if turn_on else '{"on":false}'))
