e4af24313c4eb9184f15bd0f4663d853f8f702ee
snototter <h.possegger+dev@gmail.com>
2019-09-09 00:32:30 +0200

    run bot as a service, generic service install script

---
+++ helheimr/services-register.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -
+
+# Register service
+function register_service
+{
+  svc=$1.service
+  sysvc=/etc/systemd/system/$svc
+
+  if [ -f "$sysvc" ]
+  then
+    echo "Need to stop/disable previously registered instance ($sysvc)"
+    # Stop and disable previously registered instances
+    sudo systemctl disable $svc
+    sudo systemctl stop $svc
+  fi
+  
+
+  # Create/replace the service file
+  workdir=$(pwd)
+  sudo cp services/$svc /etc/systemd/system
+  sudo sed -i "s/RAGNAROEKUSR/$USER/g" "$sysvc"
+  sudo sed -i "s|RAGNAROEKWORKDIR|$workdir|g" "$sysvc"
+
+  echo "Registering '$svc' for user '$USER'"
+
+  # Reload and enable service
+  #sudo systemctl daemon-reload
+  #sudo systemctl enable $svc
+  #sudo systemctl start $svc
+}
+
+register_service helheimr-telegram-bot
