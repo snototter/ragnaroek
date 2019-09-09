8e12e50574428e9c11c2a18e894c6ee29fb8ba5a
Horst Possegger <possegger@icg.tugraz.at>
2019-09-09 22:48:57 +0200

    basic task scheduling

---
+++ helheimr/helheimr_controller.py
@@ -0,0 +1,142 @@
+# The heart of my home automation project
+
+import threading
+import logging
+import datetime
+import time
+
+import helheimr_utils as hu
+
+class ManualHeatingJob(hu.Job):
+    def __init__(self, controller=None):
+        super(ManualHeatingJob, self).__init__(interval=24)
+        self.unit = 'hours'
+        self.controller = controller
+        self.do(self.heat_up)
+        print('Will start heating at:', self.next_run)
+        self.next_run = hu.datetime_now()
+
+    def __str__(self):
+        return "ManualHeatingJob()"
+
+    def heat_up(self):
+        i = 0
+        while self.keep_running:
+            print('I am heating')
+            time.sleep(1)
+            i += 1
+            if i > 4:
+                break
+        print('Terminating HEATER!')
+        controller.cancel_job(self)
+
+
+def dummy_job(param):
+    logging.getLogger().info('Job is running every {} sec'.format(param))
+
+class HelheimrController:
+    def __init__(self, config):
+        self.lock = threading.Lock()
+        self.condition_var = threading.Condition(self.lock)
+        self.logger = logging.getLogger()
+        self.run_loop = True
+        self.job_list = list()
+
+        self.poll_interval = 3 #TODO config
+
+        self.worker_thread = threading.Thread(target=self.control_loop)
+        self.worker_thread.start()
+
+        self.condition_var.acquire()
+        # self.job_list.append(hu.Job.every(5).seconds.do(dummy_job, 5))
+        self.job_list.append(hu.Job.every(10).seconds.do(dummy_job, 10))
+        # self.job_list.append(hu.Job.every().minute.do(self.stop))
+        self.job_list.append(ManualHeatingJob(controller=self))
+        self.job_list.append(hu.Job.every(15).seconds.do(self.stop))
+        self.condition_var.notify()
+        self.condition_var.release()
+
+        #TODO keep active heating job as separate variable
+
+
+    def stop(self):
+        self.run_loop = False
+
+
+    def join(self):
+        self.worker_thread.join()
+
+
+    @property
+    def next_run(self):
+        if len(self.job_list) == 0:
+            return None
+        return min(self.job_list).next_run
+
+
+    @property
+    def idle_time(self):
+        return hu.datetime_difference(self.next_run, datetime.datetime.utcnow()).total_seconds()
+
+    # def schedule_heating(self, interval)
+
+
+    def cancel_job(self, job):
+        # Delete a registered job
+        try:
+            self.logger.info('[HelheimrController] Removing job "{}"'.format(job))
+            self.job_list.remove(job)
+        except ValueError:
+            self.logger.error('[HelheimrController] Could not cancel job "{}"'.format(job))
+
+
+    def turn_on_manually(self, duration=None):
+        self.condition_var.acquire()
+        self.condition_var.release()
+        # self.lock.acquire()
+        # self.lock.release()
+        #TODO abort running task
+        if duration is None:
+            self.logger.info('[HelheimrController] Start heating (forever) due to user request')
+        else:
+            if duration < 0:
+                self.logger.error('[HelheimrController] Invalid duration provided, ignoring request')
+                return False
+            self.logger.info('[HelheimrController] Start heating for {} TODO hours?'.format(duration))
+        return True
+
+    def turn_off_manually(self):
+        self.logger.info('[HelheimrController] Stop heating')
+        return True
+    
+
+
+    def control_loop(self):
+        while self.run_loop:
+            self.condition_var.acquire()
+            # Query job list for scheduled/active jobs
+            runnable_jobs = (job for job in self.job_list if job.should_run)
+            for job in sorted(runnable_jobs):
+                # If there is a job which is not running already, start it.
+                if not job.is_running:
+                    job.start()
+                #TODO check if heating job is still running before invoking another!
+        
+            poll_interval = self.poll_interval if not self.job_list else min(self.poll_interval, self.idle_time)
+            self.condition_var.wait(timeout=poll_interval)
+            self.condition_var.release()
+        self.logger.info('[HelheimrController] Shutting down control loop, terminating active tasks')
+        for job in self.job_list:
+            if job.is_running:
+                job.stop()
+        self.logger.info('[HelheimrController] Clean up done, goodbye!')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, #logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+        
+    ctrl_cfg = hu.load_configuration('configs/ctrl.cfg')
+
+    controller = HelheimrController(ctrl_cfg)
+    controller.join()
--- helheimr/helheimr_utils.py
+++ helheimr/helheimr_utils.py
@@ -1,5 +1,18 @@
 import libconf
 from emoji import emojize
+import datetime
+from dateutil import tz
+try:
+    from collections.abc import Hashable
+except ImportError:
+    from collections import Hashable
+import functools
+import logging
+import random
+import re
+import time
+import threading
+
 # import urllib3 #TODO adjust https://stackoverflow.com/questions/3764291/checking-network-connection for py3
 # def internet_on():
 #     try:
@@ -31,3 +44,422 @@ def emo(txt):
 def load_configuration(filename):
     with open(filename) as f:
         return libconf.load(f)
+
+
+#######################################################################
+# Time stuff
+
+
+def as_timezone(dt_object, tz_from, tz_to):
+    if dt_object.tzinfo is None or dt_object.tzinfo.utcoffset(dt_object) is None:
+        dt_object = dt_object.replace(tzinfo=tz_from)
+    #TODO check if tzinfo matches tz_from!
+    return dt_object.astimezone(tz_to)
+
+def datetime_as_local(dt_object):
+    return as_timezone(dt_object, tz.tzutc(), tz.tzlocal())
+
+
+def datetime_as_utc(dt_object):
+    """Convert datetime object from local timezone to UTC (for 
+    scheduling, we have to take care of daylight savings time).
+    """
+    return as_timezone(dt_object, tz.tzlocal(), tz.tzutc())
+    #     from_zone = tz.tzlocal() #tz.gettz('Europe/Vienna')
+    #     utc = dt_object.replace(tzinfo=from_zone)
+    # to_zone = tz.gettz('UTC') # or tz.tzutc()
+
+
+def time_as_utc(t):
+    if t.tzinfo is None or t.tzinfo.utcoffset(t) is None:
+        dt = datetime.datetime.combine(datetime.datetime.today(), t, tzinfo=tz.tzlocal())
+    else:
+        dt = datetime.datetime.combine(datetime.datetime.today(), t, tzinfo=t.tzinfo)
+    return datetime_as_utc(dt).timetz()
+
+
+def datetime_now():
+    return datetime_as_utc(datetime.datetime.now())
+
+
+def datetime_difference(dt_object_start, dt_object_end):
+    """Converts times to UTC and returns the time delta."""
+    return datetime_as_utc(dt_object_end) - datetime_as_utc(dt_object_start)
+
+
+#######################################################################
+## Schedulable jobs (based on https://github.com/dbader/schedule, but
+## timezone aware and multi-threaded)
+class ScheduleError(Exception):
+    """Base schedule exception"""
+    pass
+
+class ScheduleValueError(ScheduleError):
+    """Base schedule value error"""
+    pass
+
+class IntervalError(ScheduleValueError):
+    """An improper interval was used"""
+    pass
+
+class CancelJob(object):
+    """Can be returned from a job to unschedule itself."""
+    pass
+
+class Job(object):
+    @staticmethod
+    def every(interval=1):
+        return Job(interval=interval)
+
+    def __init__(self, interval):
+        self.interval = interval    # pause interval * unit between runs
+        self.job_func = None        # the job job_func to run
+        self.unit = None            # time units, e.g. 'minutes', 'hours', ...
+        self.at_time = None         # optional time at which this job runs
+        self.last_run = None        # datetime of the last run
+        self.next_run = None        # datetime of the next run
+        self.period = None          # timedelta between runs, only valid for
+        self.start_day = None       # Specific day of the week to start on
+        self.tags = set()           # unique set of tags for the job
+        self.worker_thread = None
+        self.keep_running = False
+
+    def __lt__(self, other):
+        """Jobs are sortable based on the time they run next."""
+        return self.next_run < other.next_run
+
+    def __str__(self):
+        return (
+            "Job(interval={}, "
+            "unit={}, "
+            "do={}, "
+            "args={}, "
+            "kwargs={})"
+        ).format(self.interval,
+                 self.unit,
+                 self.job_func.__name__,
+                 self.job_func.args,
+                 self.job_func.keywords)
+
+    def __repr__(self):
+        def format_time(t):
+            return t.strftime('%Y-%m-%d %H:%M:%S') if t else '[never]'
+
+        def is_repr(j):
+            return not isinstance(j, Job)
+
+        timestats = '(last run: %s, next run: %s)' % (
+                    format_time(self.last_run), format_time(self.next_run))
+
+        if hasattr(self.job_func, '__name__'):
+            job_func_name = self.job_func.__name__
+        else:
+            job_func_name = repr(self.job_func)
+        args = [repr(x) if is_repr(x) else str(x) for x in self.job_func.args]
+        kwargs = ['%s=%s' % (k, repr(v))
+                  for k, v in self.job_func.keywords.items()]
+        call_repr = job_func_name + '(' + ', '.join(args + kwargs) + ')'
+
+        if self.at_time is not None:
+            return 'Every %s %s at %s do %s %s' % (
+                   self.interval,
+                   self.unit[:-1] if self.interval == 1 else self.unit,
+                   self.at_time, call_repr, timestats)
+        else:
+            fmt = (
+                'Every %(interval)s ' +
+                '%(unit)s do %(call_repr)s %(timestats)s'
+            )
+
+            return fmt % dict(
+                interval=self.interval,
+                unit=(self.unit[:-1] if self.interval == 1 else self.unit),
+                call_repr=call_repr,
+                timestats=timestats
+            )
+
+    @property
+    def is_running(self):
+        return self.worker_thread is not None and self.keep_running
+
+    @property
+    def second(self):
+        if self.interval != 1:
+            raise IntervalError('Use seconds instead of second')
+        return self.seconds
+
+    @property
+    def seconds(self):
+        self.unit = 'seconds'
+        return self
+
+    @property
+    def minute(self):
+        if self.interval != 1:
+            raise IntervalError('Use minutes instead of minute')
+        return self.minutes
+
+    @property
+    def minutes(self):
+        self.unit = 'minutes'
+        return self
+
+    @property
+    def hour(self):
+        if self.interval != 1:
+            raise IntervalError('Use hours instead of hour')
+        return self.hours
+
+    @property
+    def hours(self):
+        self.unit = 'hours'
+        return self
+
+    @property
+    def day(self):
+        if self.interval != 1:
+            raise IntervalError('Use days instead of day')
+        return self.days
+
+    @property
+    def days(self):
+        self.unit = 'days'
+        return self
+
+    @property
+    def week(self):
+        if self.interval != 1:
+            raise IntervalError('Use weeks instead of week')
+        return self.weeks
+
+    @property
+    def weeks(self):
+        self.unit = 'weeks'
+        return self
+
+    @property
+    def monday(self):
+        if self.interval != 1:
+            raise IntervalError('Use mondays instead of monday')
+        self.start_day = 'monday'
+        return self.weeks
+
+    @property
+    def tuesday(self):
+        if self.interval != 1:
+            raise IntervalError('Use tuesdays instead of tuesday')
+        self.start_day = 'tuesday'
+        return self.weeks
+
+    @property
+    def wednesday(self):
+        if self.interval != 1:
+            raise IntervalError('Use wednesdays instead of wednesday')
+        self.start_day = 'wednesday'
+        return self.weeks
+
+    @property
+    def thursday(self):
+        if self.interval != 1:
+            raise IntervalError('Use thursdays instead of thursday')
+        self.start_day = 'thursday'
+        return self.weeks
+
+    @property
+    def friday(self):
+        if self.interval != 1:
+            raise IntervalError('Use fridays instead of friday')
+        self.start_day = 'friday'
+        return self.weeks
+
+    @property
+    def saturday(self):
+        if self.interval != 1:
+            raise IntervalError('Use saturdays instead of saturday')
+        self.start_day = 'saturday'
+        return self.weeks
+
+    @property
+    def sunday(self):
+        if self.interval != 1:
+            raise IntervalError('Use sundays instead of sunday')
+        self.start_day = 'sunday'
+        return self.weeks
+
+    def tag(self, *tags):
+        """
+        Tags the job with one or more unique indentifiers.
+        Tags must be hashable. Duplicate tags are discarded.
+        :param tags: A unique list of ``Hashable`` tags.
+        :return: The invoked job instance
+        """
+        if not all(isinstance(tag, Hashable) for tag in tags):
+            raise TypeError('Tags must be hashable')
+        self.tags.update(tags)
+        return self
+
+    def at(self, time_str):
+        """
+        Specify a particular time that the job should be run at.
+        :param time_str: A string in one of the following formats: `HH:MM:SS`,
+            `HH:MM`,`:MM`, `:SS`. The format must make sense given how often
+            the job is repeating; for example, a job that repeats every minute
+            should not be given a string in the form `HH:MM:SS`. The difference
+            between `:MM` and `:SS` is inferred from the selected time-unit
+            (e.g. `every().hour.at(':30')` vs. `every().minute.at(':30')`).
+        :return: The invoked job instance
+        """
+        if (self.unit not in ('days', 'hours', 'minutes')
+                and not self.start_day):
+            raise ScheduleValueError('Invalid unit')
+        if not isinstance(time_str, str):
+            raise TypeError('at() should be passed a string')
+        if self.unit == 'days' or self.start_day:
+            if not re.match(r'^([0-2]\d:)?[0-5]\d:[0-5]\d$', time_str):
+                raise ScheduleValueError('Invalid time format')
+        if self.unit == 'hours':
+            if not re.match(r'^([0-5]\d)?:[0-5]\d$', time_str):
+                raise ScheduleValueError(('Invalid time format for'
+                                          ' an hourly job'))
+        if self.unit == 'minutes':
+            if not re.match(r'^:[0-5]\d$', time_str):
+                raise ScheduleValueError(('Invalid time format for'
+                                          ' a minutely job'))
+        time_values = time_str.split(':')
+        if len(time_values) == 3:
+            hour, minute, second = time_values
+        elif len(time_values) == 2 and self.unit == 'minutes':
+            hour = 0
+            minute = 0
+            _, second = time_values
+        else:
+            hour, minute = time_values
+            second = 0
+        if self.unit == 'days' or self.start_day:
+            hour = int(hour)
+            if not (0 <= hour <= 23):
+                raise ScheduleValueError('Invalid number of hours')
+        elif self.unit == 'hours':
+            hour = 0
+        elif self.unit == 'minutes':
+            hour = 0
+            minute = 0
+        minute = int(minute)
+        second = int(second)
+        self.at_time = time_as_utc(datetime.time(hour, minute, second))
+        return self
+
+    def do(self, job_func, *args, **kwargs):
+        """
+        Specifies the job_func that should be called every time the
+        job runs.
+        Any additional arguments are passed on to job_func when
+        the job runs.
+        :param job_func: The function to be scheduled
+        :return: The invoked job instance
+        """
+        self.job_func = functools.partial(job_func, *args, **kwargs)
+        try:
+            functools.update_wrapper(self.job_func, job_func)
+        except AttributeError:
+            # job_funcs already wrapped by functools.partial won't have
+            # __name__, __module__ or __doc__ and the update_wrapper()
+            # call will fail.
+            pass
+        self._schedule_next_run()
+        return self
+
+    @property
+    def should_run(self):
+        """
+        :return: ``True`` if the job should be run now.
+        """
+        return datetime_now() >= self.next_run
+
+    def start(self):
+        """Start the job thread and immediately reschedule this job."""
+        # logging.getLogger().info('Running job %s', self)
+
+        # ret = self.job_func() #TODO start thread, add variables to keep running, etc
+        if self.worker_thread is not None and self.keep_running:
+            logging.getLogger().warn('This job is already running. I will ignore this additional start() request: {}'.format(self))
+        else:    
+            self.keep_running = True
+            self.worker_thread = threading.Thread(target=self._run_blocking, daemon=True)
+            self.worker_thread.start()
+            self._schedule_next_run()
+            self.last_run = datetime_now()
+
+    def _run_blocking(self):
+        self.job_func()
+        self.keep_running = False
+
+    def stop(self):
+        if self.keep_running:
+            self.keep_running = False
+            self.worker_thread.join()#TODO timeout
+
+    def _schedule_next_run(self):
+        """
+        Compute the instant when this job should run next.
+        """
+        if self.unit not in ('seconds', 'minutes', 'hours', 'days', 'weeks'):
+            raise ScheduleValueError('Invalid unit')
+
+        interval = self.interval
+
+        self.period = datetime.timedelta(**{self.unit: interval})
+        self.next_run = datetime_now() + self.period
+        if self.start_day is not None:
+            if self.unit != 'weeks':
+                raise ScheduleValueError('`unit` should be \'weeks\'')
+            weekdays = (
+                'monday',
+                'tuesday',
+                'wednesday',
+                'thursday',
+                'friday',
+                'saturday',
+                'sunday'
+            )
+            if self.start_day not in weekdays:
+                raise ScheduleValueError('Invalid start day')
+            weekday = weekdays.index(self.start_day)
+            days_ahead = weekday - self.next_run.weekday()
+            if days_ahead <= 0:  # Target day already happened this week
+                days_ahead += 7
+            self.next_run += datetime.timedelta(days_ahead) - self.period
+        if self.at_time is not None:
+            if (self.unit not in ('days', 'hours', 'minutes')
+                    and self.start_day is None):
+                raise ScheduleValueError(('Invalid unit without'
+                                          ' specifying start day'))
+            kwargs = {
+                'second': self.at_time.second,
+                'microsecond': 0
+            }
+            if self.unit == 'days' or self.start_day is not None:
+                kwargs['hour'] = self.at_time.hour
+            if self.unit in ['days', 'hours'] or self.start_day is not None:
+                kwargs['minute'] = self.at_time.minute
+            self.next_run = self.next_run.replace(**kwargs)
+            # If we are running for the first time, make sure we run
+            # at the specified time *today* (or *this hour*) as well
+            if not self.last_run:
+                now = timdatetime.datetime.now()
+                if (self.unit == 'days' and self.at_time > now.time() and
+                        self.interval == 1):
+                    self.next_run = self.next_run - datetime.timedelta(days=1)
+                elif self.unit == 'hours' \
+                        and self.at_time.minute > now.minute \
+                        or (self.at_time.minute == now.minute
+                            and self.at_time.second > now.second):
+                    self.next_run = self.next_run - datetime.timedelta(hours=1)
+                elif self.unit == 'minutes' \
+                        and self.at_time.second > now.second:
+                    self.next_run = self.next_run - \
+                                    datetime.timedelta(minutes=1)
+        if self.start_day is not None and self.at_time is not None:
+            # Let's see if we will still make that time we specified today
+            if (self.next_run - datetime.datetime.now()).days >= 7:
+                self.next_run -= self.period
